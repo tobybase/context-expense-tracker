f92e5837ec2a06a912c418e55e4f699cc9b0ce60
tobybase <74760266+tobybase@users.noreply.github.com>
2022-04-21 15:58:24 +0800

    adding context API

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 import './App.css';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className='container'>
         <Balance />
@@ -16,7 +17,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const setTextHandler = (event) => {
+    event.preventDefault();
+    setText(event.target.value);
+    setText('');
+  };
+
+  const setAmountHandler = (event) => {
+    event.preventDefault();
+    setAmount(event.target.value);
+    setAmount('');
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form>
         <div className='form-control'>
           <label htmlFor='text'>Text</label>
-          <input type='text' id='text' placeholder='Enter text...' />
+          <input
+            type='text'
+            text={text}
+            onChange={setTextHandler}
+            placeholder='Enter text...'
+          />
         </div>
         <div className='form-control'>
           <label htmlFor='amount'>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type='number' placeholder='Enter amount...' />
+          <input
+            type='number'
+            text={amount}
+            onChange={setAmountHandler}
+            placeholder='Enter amount...'
+          />
         </div>
         <button className='btn'>Add transaction</button>
       </form>
